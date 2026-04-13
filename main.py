f2bd0bd674c2359c8c35a7347b252cfdc82c330d
SakibSibly <sibly@duck.com>
2026-04-13 13:00:23 +0600

    Update "All execs added to main"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from nogod import run_nogod
+from nagad import run_nagad
 from bkash import run_bkash
 from billing_system import run_billing_system
 from ssl_payment import run_ssl 
@@ -6,7 +6,7 @@ from ssl_payment import run_ssl
 def main():
     print("🚀 Starting Automation...")
     run_billing_system()
-    # run_nogod()
+    run_nagad()
     run_bkash()
     run_ssl()
 
