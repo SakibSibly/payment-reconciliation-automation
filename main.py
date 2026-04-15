91c07f3bfe8bf872046d99d6e17073218be76035
SakibSibly <sibly@duck.com>
2026-04-15 13:24:20 +0600

    Add "Upload and compare function added"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from nagad.nagad import run_nagad
 from bkash.pgw.bkash import run_bkash
 from billing_system import run_billing_system
-from ssl_payment import run_ssl 
+from ssl_payment import run_ssl
+from utils.reconcile_upload import run_upload
 
 def main():
     print("🚀 Starting Automation...")
@@ -9,6 +10,9 @@ def main():
     run_nagad()
     run_bkash()
     run_ssl()
+
+    print("🚀 Starting Upload and Comparison...")
+    run_upload()
 
     print("✅ All tasks completed!")
 
---
+++ utils/reconcile_upload.py
@@ -0,0 +1,5 @@
+def run_upload():
+    pass
+
+if __name__ == "__main__":
+    run_upload()
